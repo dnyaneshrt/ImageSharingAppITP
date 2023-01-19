4a3a6ed8fa300999ef43290d7460ea5931ae7072
Dnyaneshwar <sartape51@gmail.com>
2023-01-18 20:04:12 -0800

    Volley API call

--- app/src/main/java/com/example/imagesharingapp/MainActivity.java
+++ app/src/main/java/com/example/imagesharingapp/MainActivity.java
@@ -1,9 +1,22 @@
 package com.example.imagesharingapp;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
@@ -17,11 +30,21 @@ import com.example.imagesharingapp.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
+    ProgressBar progressBar;
+    Button btnLoadImages;
+    ImageView imageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +54,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         setSupportActionBar(binding.toolbar);
+        progressBar=findViewById(R.id.progressBar);
+        btnLoadImages=findViewById(R.id.btn_loadImages);
+        imageView=findViewById(R.id.imageView);
 
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
-        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        getImages();
+
+        btnLoadImages.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getImages();
+            }
+        });
+
 
         binding.fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -43,6 +75,51 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+    }
+
+    private void getImages() {
+        String url = "https://meme-api.com/gimme";
+
+        progressBar.setVisibility(View.VISIBLE);
+       RequestQueue mRequestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                    @Override
+                    public void onResponse(JSONObject response) {
+//                        Toast.makeText(MainActivity.this, "output: "+response.toString(), Toast.LENGTH_LONG).show();
+                        String url = null;
+                        try {
+                            url = response.getString("url");
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                        Glide.with(MainActivity.this).load(url).addListener(new RequestListener<Drawable>() {
+                            @Override
+                            public boolean onLoadFailed(@Nullable GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+
+                            @Override
+                            public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                                progressBar.setVisibility(View.GONE);
+                                return false;
+                            }
+                        }).into(imageView);
+                    }
+                }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+
+                    }
+                });
+
+        mRequestQueue.add(jsonObjectRequest);
+
     }
 
     @Override
@@ -67,10 +144,5 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
-        return NavigationUI.navigateUp(navController, appBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+
 }
